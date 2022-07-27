1715711e55380245712d050e5327b6356d909121
X3CP4o2 <x3cp4o2@gmail.com>
2022-07-27 13:06:26 +0200

    Solutions

---
+++ clippy/target/debug/deps/clippy3-0b8d7e3130b28049.d
@@ -0,0 +1,7 @@
+C:\Users\Schloerk\Desktop\VoxelRaytracing\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-0b8d7e3130b28049.rmeta: clippy3.rs
+
+C:\Users\Schloerk\Desktop\VoxelRaytracing\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-0b8d7e3130b28049.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
